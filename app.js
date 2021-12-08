79b718789fea2d195fb1002ba1025057dd5ac475
Benatovich <sambenatovich@gmail.com>
2021-12-08 14:10:58 -0500

    render functions written, digging into app.js now

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
+import { renderOption } from './render-utils.js';
+import { renderPoll } from './render-utils.js';
 
 // let state
-
+const pastPollsArray = [];
+let currentPoll = {
+    nameA: '',
+    nameB: '',
+    scoreA: 0,
+    scoreB: 0,
+};
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+console.log(renderOption());
+console.log(renderPoll());
---
+++ render-utils.js
@@ -0,0 +1,43 @@
+// Q: can I put all of my functions here?
+// A: can anyone stop me...?
+
+// PURE FUNCTIONS
+export function renderPoll(poll) {
+    const pollDiv = document.createElement('div');
+    const optionADiv = renderOption(poll.nameA, poll.scoreA);
+    // ^I'm tempted to do this with pollA.name instead of poll.nameA but this should be easier for now
+    const optionBDiv = renderOption(poll.nameB, poll.scoreB);
+
+    pollDiv.append(optionADiv, optionBDiv);
+
+    pollDiv.classList.add('poll');
+
+    return pollDiv;
+}
+
+export function renderOption(name, score) {
+    const optionDiv = document.createElement('div');
+    const nameDiv = document.createElement('p');
+    const scoreDiv = document.createElement('p');
+
+    optionDiv.classList.add('option');
+    nameDiv.classList.add('name');
+    scoreDiv.classList.add('score');
+
+    nameDiv.textContent = name;
+    scoreDiv.textContent = score;
+
+    optionDiv.append(nameDiv, scoreDiv);
+
+    return optionDiv;
+}
+
+// IMPURE FUNCTIONS
+
+export function displayCurrentPoll() {
+
+}
+
+export function displayAllPolls() {
+
+}
